5dce15c72de808a3d5f414297b356e1f8ea57292
AndreyLyre <andreygabrielbenchea@gmail.com>
2024-09-08 21:08:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ document.getElementById('countdownForm').addEventListener('submit', function(eve
     // Get input values
     const text = document.getElementById('text').value;
     const date = new Date(document.getElementById('date').value);
+    const time = document.getElementById('time').value;
     
+    // Set time to the date object
+    const [hours, minutes] = time.split(':').map(Number);
+    date.setHours(hours, minutes, 0, 0); // Set hours, minutes, seconds, milliseconds
+
     // Validate date
     if (isNaN(date.getTime())) {
         alert('Invalid date');
